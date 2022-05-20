c445b81805f71a34959e75b56fc34a6572f81030
Zecky-dev <l2021012041@ogr.sdu.edu.tr>
2022-05-21 01:19:03 +0300

    Version 1.0.0

---
+++ app/src/main/java/com/zecky_dev/passwordgenerator/MainActivity.java
@@ -0,0 +1,145 @@
+package com.zecky_dev.passwordgenerator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText passLengthET,generatedPasswordET;
+    private CheckBox lowerCaseCB,upperCaseCB,numbersCB,specialCharsCB;
+    private ArrayList<Boolean> checkedCheckBoxes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String lowercase_letters = "abcdefghijklmnoprstuvyzwx";
+        char[] lowercase_letters_arr = lowercase_letters.toCharArray();
+        String uppercase_letters = "ABCDEFGHIJKLMNOPRSTUVYZX";
+        char[] uppercase_letters_arr = uppercase_letters.toCharArray();
+        String specialCharacters = " !\"#$%&'%()*+,-./:;<=>?@[\\]^_`{|}~";
+        char[] specialChars_arr = specialCharacters.toCharArray();
+        int[] numbers_arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        checkedCheckBoxes = new ArrayList<>();
+        lowerCaseCB = findViewById(R.id.lowerCaseCB);
+        upperCaseCB = findViewById(R.id.upperCaseCB);
+        numbersCB = findViewById(R.id.numbersCB);
+        specialCharsCB = findViewById(R.id.specialCharactersCB);
+        passLengthET = findViewById(R.id.passwordLengthET);
+        generatedPasswordET = findViewById(R.id.generatedPassET);
+        Button generatePassBTN = findViewById(R.id.generatePassBTN);
+        Random random = new Random();
+        ArrayList<String> checkedBoxes = new ArrayList<>();
+        checkedBoxes.add("lowerCase");
+        lowerCaseCB.setChecked(true);
+        lowerCaseCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkedBoxes.add("lowerCase");
+                }
+                else{
+                    checkedBoxes.remove("lowerCase");
+                }
+                System.out.println(checkedBoxes.size());
+            }
+        });
+        upperCaseCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkedBoxes.add("upperCase");
+                }
+                else{
+                    checkedBoxes.remove("upperCase");
+                }
+                System.out.println(checkedBoxes.size());
+            }
+        });
+        numbersCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkedBoxes.add("numbers");
+                }
+                else{
+                    checkedBoxes.remove("numbers");
+                }
+                System.out.println(checkedBoxes.size());
+            }
+        });
+        specialCharsCB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    checkedBoxes.add("specialChars");
+                }
+                else{
+                    checkedBoxes.remove("specialChars");
+                }
+                System.out.println(checkedBoxes.size());
+            }
+        });
+        generatePassBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkedBoxes.size()==0){
+                    Toast.makeText(MainActivity.this, "You must check at least 1 option.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(!passLengthET.getText().toString().isEmpty()){
+                        int passLength,randomOption,randomIndex;
+                        String generatedPassword = "";
+                        passLength = Integer.parseInt(passLengthET.getText().toString());
+                        for(int i=0;i<passLength;i++){
+                            randomOption = random.nextInt(checkedBoxes.size());
+                            System.out.println(randomOption);
+                            if(checkedBoxes.get(randomOption).equals("lowerCase")){
+                                randomIndex = random.nextInt(lowercase_letters.length());
+                                generatedPassword+=String.valueOf(lowercase_letters_arr[randomIndex]);
+                            }
+                            if(checkedBoxes.get(randomOption).equals("upperCase")){
+                                randomIndex = random.nextInt(uppercase_letters.length());
+                                generatedPassword += String.valueOf(uppercase_letters_arr[randomIndex]);
+                            }
+                            if(checkedBoxes.get(randomOption).equals("numbers")){
+                                randomIndex = random.nextInt(numbers_arr.length);
+                                generatedPassword += String.valueOf(numbers_arr[randomIndex]);
+                            }
+                            if(checkedBoxes.get(randomOption).equals("specialChars")){
+                                randomIndex = random.nextInt(specialChars_arr.length);
+                                generatedPassword += String.valueOf(specialChars_arr[randomIndex]);
+                            }
+                            generatedPasswordET.setText(generatedPassword);
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Please fill blank area!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+
+
+
+
+    }
+}
